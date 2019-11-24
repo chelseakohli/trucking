e87b8dc3757f6e787c89b9e01b2bae89dea9a193
Dilpreet Kohli <dilpreet.kohli@anakage.com>
2019-11-24 21:22:23 +0530

    first commit

---
+++ src/test/javascript/e2e/entities/bank-account/bank-account-update.page-object.ts
@@ -0,0 +1,61 @@
+import { element, by, ElementFinder } from 'protractor';
+
+export default class BankAccountUpdatePage {
+  pageTitle: ElementFinder = element(by.id('jhipsterSampleApplicationReactApp.bankAccount.home.createOrEditLabel'));
+  saveButton: ElementFinder = element(by.id('save-entity'));
+  cancelButton: ElementFinder = element(by.id('cancel-save'));
+  nameInput: ElementFinder = element(by.css('input#bank-account-name'));
+  balanceInput: ElementFinder = element(by.css('input#bank-account-balance'));
+  userSelect: ElementFinder = element(by.css('select#bank-account-user'));
+
+  getPageTitle() {
+    return this.pageTitle;
+  }
+
+  async setNameInput(name) {
+    await this.nameInput.sendKeys(name);
+  }
+
+  async getNameInput() {
+    return this.nameInput.getAttribute('value');
+  }
+
+  async setBalanceInput(balance) {
+    await this.balanceInput.sendKeys(balance);
+  }
+
+  async getBalanceInput() {
+    return this.balanceInput.getAttribute('value');
+  }
+
+  async userSelectLastOption() {
+    await this.userSelect
+      .all(by.tagName('option'))
+      .last()
+      .click();
+  }
+
+  async userSelectOption(option) {
+    await this.userSelect.sendKeys(option);
+  }
+
+  getUserSelect() {
+    return this.userSelect;
+  }
+
+  async getUserSelectedOption() {
+    return this.userSelect.element(by.css('option:checked')).getText();
+  }
+
+  async save() {
+    await this.saveButton.click();
+  }
+
+  async cancel() {
+    await this.cancelButton.click();
+  }
+
+  getSaveButton() {
+    return this.saveButton;
+  }
+}
---
+++ src/test/javascript/e2e/entities/bank-account/bank-account.spec.ts
@@ -0,0 +1,90 @@
+import { browser, element, by } from 'protractor';
+
+import NavBarPage from './../../page-objects/navbar-page';
+import SignInPage from './../../page-objects/signin-page';
+import BankAccountComponentsPage, { BankAccountDeleteDialog } from './bank-account.page-object';
+import BankAccountUpdatePage from './bank-account-update.page-object';
+import { waitUntilDisplayed, waitUntilHidden } from '../../util/utils';
+
+const expect = chai.expect;
+
+describe('BankAccount e2e test', () => {
+  let navBarPage: NavBarPage;
+  let signInPage: SignInPage;
+  let bankAccountComponentsPage: BankAccountComponentsPage;
+  let bankAccountUpdatePage: BankAccountUpdatePage;
+  let bankAccountDeleteDialog: BankAccountDeleteDialog;
+
+  before(async () => {
+    await browser.get('/');
+    navBarPage = new NavBarPage();
+    signInPage = await navBarPage.getSignInPage();
+    await signInPage.waitUntilDisplayed();
+
+    await signInPage.username.sendKeys('admin');
+    await signInPage.password.sendKeys('admin');
+    await signInPage.loginButton.click();
+    await signInPage.waitUntilHidden();
+    await waitUntilDisplayed(navBarPage.entityMenu);
+  });
+
+  it('should load BankAccounts', async () => {
+    await navBarPage.getEntityPage('bank-account');
+    bankAccountComponentsPage = new BankAccountComponentsPage();
+    expect(await bankAccountComponentsPage.getTitle().getText()).to.match(/Bank Accounts/);
+  });
+
+  it('should load create BankAccount page', async () => {
+    await bankAccountComponentsPage.clickOnCreateButton();
+    bankAccountUpdatePage = new BankAccountUpdatePage();
+    expect(await bankAccountUpdatePage.getPageTitle().getAttribute('id')).to.match(
+      /jhipsterSampleApplicationReactApp.bankAccount.home.createOrEditLabel/
+    );
+    await bankAccountUpdatePage.cancel();
+  });
+
+  it('should create and save BankAccounts', async () => {
+    async function createBankAccount() {
+      await bankAccountComponentsPage.clickOnCreateButton();
+      await bankAccountUpdatePage.setNameInput('name');
+      expect(await bankAccountUpdatePage.getNameInput()).to.match(/name/);
+      await bankAccountUpdatePage.setBalanceInput('5');
+      expect(await bankAccountUpdatePage.getBalanceInput()).to.eq('5');
+      await bankAccountUpdatePage.userSelectLastOption();
+      await waitUntilDisplayed(bankAccountUpdatePage.getSaveButton());
+      await bankAccountUpdatePage.save();
+      await waitUntilHidden(bankAccountUpdatePage.getSaveButton());
+      expect(await bankAccountUpdatePage.getSaveButton().isPresent()).to.be.false;
+    }
+
+    await createBankAccount();
+    await bankAccountComponentsPage.waitUntilLoaded();
+    const nbButtonsBeforeCreate = await bankAccountComponentsPage.countDeleteButtons();
+    await createBankAccount();
+
+    await bankAccountComponentsPage.waitUntilDeleteButtonsLength(nbButtonsBeforeCreate + 1);
+    expect(await bankAccountComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeCreate + 1);
+  });
+
+  it('should delete last BankAccount', async () => {
+    await bankAccountComponentsPage.waitUntilLoaded();
+    const nbButtonsBeforeDelete = await bankAccountComponentsPage.countDeleteButtons();
+    await bankAccountComponentsPage.clickOnLastDeleteButton();
+
+    const deleteModal = element(by.className('modal'));
+    await waitUntilDisplayed(deleteModal);
+
+    bankAccountDeleteDialog = new BankAccountDeleteDialog();
+    expect(await bankAccountDeleteDialog.getDialogTitle().getAttribute('id')).to.match(
+      /jhipsterSampleApplicationReactApp.bankAccount.delete.question/
+    );
+    await bankAccountDeleteDialog.clickOnConfirmButton();
+
+    await bankAccountComponentsPage.waitUntilDeleteButtonsLength(nbButtonsBeforeDelete - 1);
+    expect(await bankAccountComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeDelete - 1);
+  });
+
+  after(async () => {
+    await navBarPage.autoSignOut();
+  });
+});
